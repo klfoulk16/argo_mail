1ee48f0fc91ae20a5b2d3eb120da1af5be770a33
Kelly Foulk <klfoulk16@gmail.com>
2021-02-04 11:09:50 -0500

    Set up all essential mailchimp api functionality

--- mailchimp.py
+++ mailchimp.py
@@ -2,6 +2,7 @@ from mailchimp_marketing import Client
 from mailchimp_marketing.api_client import ApiClientError
 import os
 from dotenv import load_dotenv
+import hashlib
 
 
 def get_client():
@@ -53,3 +54,83 @@ def add_subscriber():
     except ApiClientError as error:
         print(f"An exception occurred: {error.text}")
 
+
+def add_or_update_subscriber():
+    list_id = "d73bda636d"
+    email = "mock1@mock.com"
+    subscriber_info = {
+        "email_address": email,
+        "status_if_new": "subscribed",
+        "merge_fields": {
+            "FNAME": "Prudence",
+            "LNAME": "McVankab",
+            "MMERGE3": 0,  # user id
+            "MMERGE6": "apple",  # provider (changed)
+            "MMERGE8": "mock1"  # username
+        }
+    }
+    subscriber_hash = hashlib.md5(email.encode('utf-8')).hexdigest()
+    client = get_client()
+    try:
+        response = client.lists.set_list_member(
+            list_id,
+            subscriber_hash,
+            subscriber_info
+        )
+        print(f"response: {response}")
+    except ApiClientError as error:
+        print(f"An exception occurred: {error.text}")
+
+
+def view_contacts_tags():
+    list_id = "d73bda636d"
+    email = "klf16@my.fsu.edu"
+    subscriber_hash = hashlib.md5(email.encode('utf-8')).hexdigest()
+
+    client = get_client()
+    try:
+        response = client.lists.get_list_member_tags(list_id, subscriber_hash)
+        print(f"response: {response}")
+    except ApiClientError as error:
+        print(f"An exception occurred: {error.text}")
+
+
+def bulk_tag():
+    tag_id = "649346"
+    list_id = "d73bda636d"
+    client = get_client()
+    try:
+        response = client.lists.batch_segment_members({
+            "members_to_add": [
+                "mock1@mock.com",
+                "klf16@my.fsu.edu"
+                ]
+            },
+            list_id,
+            tag_id
+        )
+        print(f"response: {response}")
+    except ApiClientError as error:
+        print(f"An exception occurred: {error.text}")
+
+
+def bulk_subscribe():
+    list_id = "d73bda636d"
+    member_info = {
+        "email_address": "mock1@mock.com",
+        "status": "subscribed",
+        "merge_fields": {
+          "FNAME": "Prudence",
+          "LNAME": "McVankab",
+          "MMERGE3": 0,  # user id
+          "MMERGE6": "facebook",  # provider
+          "MMERGE8": "magicmock1"  # username
+        },
+    }
+    client = get_client()
+    try:
+        # can only add up to 500 at a time
+        response = client.lists.batch_list_members(list_id, {"members": [member_info], "update_existing":True})
+        print(f"response: {response}")
+    except ApiClientError as error:
+        print(f"An exception occurred: {error.text}")
